e7686961f7b98fc7f2d2335fcdd5a6b58b0a37f8
Armaan <armaan110@gmail.com>
2014-04-18 19:02:06 -0400

    churnd js file

---
+++ churnd.js
@@ -0,0 +1,71 @@
+
+
+
+function Churnd(project_id) {
+  this.project_id = project_id;
+}
+
+Churnd.prototype.project_id = '';
+
+Churnd.prototype.login = function (email_id, account_name) {
+
+    var sendData = 
+        {
+            key: this.project_id, 
+            email: email_id,
+            account: account_name
+        };
+    var xhr = new XMLHttpRequest();   // new HttpRequest instance 
+    xhr.open("POST", "http://vast-savannah-8474.herokuapp.com/api/login");
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify(sendData));
+
+
+    xhr.onreadystatechange=function(){
+       if (xhr.readyState==4 && xhr.status==200){
+          console.log('xhr.readyState=',xhr.readyState);
+          console.log('xhr.status=',xhr.status);
+          console.log('response=',xhr.responseText);
+          // TBD
+       }
+       else
+       {
+        console.log(xhr.statusText);
+       }
+    }
+  
+};
+
+
+Churnd.prototype.track = function (email_id, interaction_type, account_name) {
+
+    var sendData = 
+        {
+            key: this.project_id, 
+            email: email_id,
+            type: interaction_type,
+            account: account_name
+        };
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    xmlhttp.open("POST", "http://vast-savannah-8474.herokuapp.com/api/track");
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify(sendData));
+  
+};
+
+Churnd.prototype.logout = function(email_id, account_name) {
+
+
+    var sendData = 
+        {
+            key: this.project_id, 
+            email: email_id,
+            account: account_name
+        };
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    xmlhttp.open("POST", "http://vast-savannah-8474.herokuapp.com/api/logout");
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify(sendData));
+
+
+};
